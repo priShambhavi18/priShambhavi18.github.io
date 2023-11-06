8b6657dab473cb9c45b53038a5d278fc0610bc93
Shambhavi Priya <103560532+priShambhavi18@users.noreply.github.com>
2023-11-06 22:19:13 +0530

    main.js

--- main.js
+++ main.js
@@ -6,30 +6,6 @@ var typed = new Typed(".text", {
     loop: true
 });
 
-// Get the menu icon and the navbar by their IDs
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar'); // Use a class selector for navbar
-
-// Add a click event listener to the menu icon
-menuIcon.addEventListener('click', function () {
-    // Toggle the visibility of the navbar
-    if (navbar.style.display === 'block') {
-        navbar.style.display = 'none';
-    } else {
-        navbar.style.display = 'block';
-    }
-});
-
-let navLinks = navbar.querySelectorAll('a');
-
-// Add a click event listener to each link in the navbar
-navLinks.forEach(function (link) {
-    link.addEventListener('click', function () {
-        if (window.innerWidth < 800) {
-            navbar.style.display = 'none';
-        }
-    });
-});
 
 
 
